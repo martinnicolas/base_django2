9fd93bf897ad8e1b1370f966a74b3a59936c340d
martinnicolas <ttincho77@gmail.com>
2018-12-16 20:46:07 -0300

    Agrego modelos, migraciones y panel de administracion

--- personas/models.py
+++ personas/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class TipoDocumento(models.Model):
+	descripcion = models.CharField(max_length=200)
+	# to string
+	def __str__(self):
+		return self.descripcion
+
+class Provincia(models.Model):
+	nombre = models.CharField(max_length=200)
+	# to string
+	def __str__(self):
+		return self.nombre
+
+class Localidad(models.Model):
+	nombre = models.CharField(max_length=200)
+	provincia = models.ForeignKey(Provincia, on_delete=models.CASCADE)
+	# to string
+	def __str__(self):
+		return self.nombre
+
+class Persona(models.Model):
+	tipo_documento = models.ForeignKey(TipoDocumento, on_delete=models.CASCADE)
+	numero_documento = models.IntegerField(blank=False, null=False)
+	apellido = models.CharField(max_length=200)
+	nombre = models.CharField(max_length=200)
+	localidad = models.ForeignKey(Localidad, on_delete=models.CASCADE)
+	# to string
+	def __str__(self):
+		return self.apellido
