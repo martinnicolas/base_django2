dd6f3224b6f6d2883422274b8cc4b606ceb290c2
martinnicolas <ttincho77@gmail.com>
2018-12-16 20:02:00 -0300

    Agrego conexion a postgresql, psycopg2 y migraciones

--- base_django2/settings.py
+++ base_django2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,9 +76,16 @@ WSGI_APPLICATION = 'base_django2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'base_django2',
+        'USER': 'postgres',
+        'PASSWORD': 'chacho77',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    },
+    'OPTIONS': {
+        'isolation_level': psycopg2.extensions.ISOLATION_LEVEL_SERIALIZABLE,
+    },
 }
 
 
